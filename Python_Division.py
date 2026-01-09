673a6b7888afc8a39f0f3554479b22fb85ccb235
shuvo01740240279 <belayethosenshuvo9@gmail.com>
2026-01-09 16:03:37 +0600

    Python: Division

---
+++ Python_Division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':    
+    a = int(input())
+    b = int(input())
+    
+    integer_division = a // b
+    fload_division = a / b
+    
+    print(f"{integer_division}")
+    print(f"{fload_division:.11f}")
